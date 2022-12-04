51849850786968b84523b7d163a9c321b518220b
MathIzuki <carcenacmathis81@gmail.com>
2022-12-04 16:07:34 +0100

    JS-Added-6e-Commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+function openThibault(){
+  document.getElementById("profil-Thibault").style.top = "650px";
+}
+
+function closeThibault(){
+  document.getElementById("profil-Thibault").style.top = "-1000px";
+}
+
+function openLea(){
+  document.getElementById("profil-Lea").style.top = "650px";
+}
+
+function closeLea(){
+  document.getElementById("profil-Lea").style.top = "-1000px";
+}
