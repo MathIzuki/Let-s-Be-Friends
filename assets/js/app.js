fe9a8a531c69aa449f247875b329967dcf655f62
MathIzuki <carcenacmathis81@gmail.com>
2022-12-04 17:39:26 +0100

    JS-Added-6.2e-Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,19 @@ function openLea(){
 function closeLea(){
   document.getElementById("profil-Lea").style.top = "-1000px";
 }
+
+function openLucas(){
+  document.getElementById("profil-Lucas").style.top = "650px";
+}
+
+function closeLucas(){
+  document.getElementById("profil-Lucas").style.top = "-1000px";
+}
+
+function openJulie(){
+  document.getElementById("profil-Julie").style.top = "650px";
+}
+
+function closeJulie(){
+  document.getElementById("profil-Julie").style.top = "-1000px";
+}
